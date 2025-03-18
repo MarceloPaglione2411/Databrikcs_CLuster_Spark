97600f8d5a23bee672196acdadd41d41d34f1194
Marcelo Paglione <104773898+MarceloPaglione2411@users.noreply.github.com>
2025-03-18 11:02:16 -0300

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,24 @@
+variable "rg_name" {
+    description = "Nome Resource Group (DATABRIKS)"
+    type = string
+}
+
+variable "rg_location" {
+    description = "Localização RG"
+    type = string
+}
+
+variable "databricks_name" {
+    description = "NOme Databricks Workspace"
+    type = string
+}
+
+variable "sku" {
+    description = "SKU  Tier Databricks Workspace"
+    type = string
+}
+
+variable "cluster_name" {
+    description = "Nome Cluster Databricks "
+    type = string
+}
